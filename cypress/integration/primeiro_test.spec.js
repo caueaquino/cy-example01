71656628028c3995579778c64fbcb8d356440f14
caue <caue@can.caue>
2020-08-12 17:24:02 -0400

    feat: implementa teste calculo de imposto

--- cypress/integration/primeiro_test.spec.js
+++ cypress/integration/primeiro_test.spec.js
@@ -5,19 +5,23 @@ describe('Calculadora de imposto', () => {
         cy.get('[name=idade]').type('20');
         cy.get('[name=sexo]').select('M');
         cy.get('[name=salario]').type('2000');
-        // cy.get(`[value='Limpar campos']`).click();
-
+        cy.get(`[value='Limpar campos']`).click();
         cy.get('[name=nome]').should('have.text', '');
         cy.get('[name=idade]').should('have.text', '');
         cy.get('[name=sexo]').should('have.value', '-');
         cy.get('[name=salario]').should('have.text', '');
     });
 
+    
     it('deve calcular o imposto devido', () => {
-        // preencha todos os campos
-
-        // clique no botao calcular
-
-        // verifique (usando .should) a saída retornada com o calculo do imposto devido
+        cy.visit('https://andreendo.github.io/sample-ui-compras/index-2.html');
+        cy.get('[name=nome]').type('Cauê');
+        cy.get('[name=idade]').type('23');
+        cy.get('[name=sexo]').select('M');
+        cy.get('[name=salario]').type('5000');
+        cy.get(`[value='Calcular']`).click();
+        cy.get('#divValorImposto').should((valorImposto) => {
+            expect(valorImposto).to.contain('750');
+        })
     });
 });
